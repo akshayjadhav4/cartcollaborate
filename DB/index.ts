e9498eb39f5f170055b764acbcc8d6fcaceefd6f
Akshay Jadhav <akshayjadhav461999@gmail.com>
2024-07-13 21:28:13 +0530

    watermelondb setup

---
+++ DB/index.ts
@@ -0,0 +1,24 @@
+import { Database } from "@nozbe/watermelondb";
+import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
+
+import schema from "./schema";
+import migrations from "./migrations";
+import AuthSession from "./model/AuthSession";
+
+const adapter = new SQLiteAdapter({
+  schema,
+  migrations,
+
+  jsi: true,
+  onSetUpError: (error) => {
+    // Database failed to load -- offer the user to reload the app or log out
+    console.log("DATABASE SETUP ERROR", error);
+  },
+});
+
+const database = new Database({
+  adapter,
+  modelClasses: [AuthSession],
+});
+
+export default database;
