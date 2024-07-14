ab8b21732afbd70678ade260bfd14ca82351bd07
Akshay Jadhav <akshayjadhav461999@gmail.com>
2024-07-14 15:32:17 +0530

    Secure auth session

--- supabase/ClientAuthStorage.ts
+++ supabase/ClientAuthStorage.ts
@@ -1,6 +1,9 @@
 import { Database } from "@nozbe/watermelondb";
 import database from "../DB";
 import AuthSession from "../DB/model/AuthSession";
+import * as Crypto from "expo-crypto";
+import * as SecureStore from "expo-secure-store";
+import * as aesjs from "aes-js";
 
 /**
  * AnyFunction, MaybePromisify, SupportedStorage code taken from node_modules/@supabase/auth-js/src/lib/types.ts
@@ -42,19 +45,21 @@ class SupabaseClientStorage implements SupportedStorage {
     return this.db
       .get<AuthSession>("auth_session")
       .find(key)
-      .then((result) => {
-        return result.session;
+      .then(async (result) => {
+        const decryptedValue = await this._decrypt(key, result.session);
+        return decryptedValue;
       })
       .catch(() => null);
   }
 
   async setItem(key: string, value: string): Promise<void> {
+    const encryptedValue = await this._encrypt(key, value);
     await this.db.write(async () => {
       await this.db
         .get<AuthSession>("auth_session")
         .create((record) => {
           record._raw.id = key;
-          record.session = value;
+          record.session = encryptedValue;
         })
         .catch((error) => {});
     });
@@ -66,9 +71,48 @@ class SupabaseClientStorage implements SupportedStorage {
       if (session) {
         await this.db.write(async () => {
           await session.destroyPermanently();
+          await SecureStore.deleteItemAsync(key);
         });
       }
     } catch (error) {}
+  }
+
+  private async _encrypt(key: string, value: string) {
+    // 128-bit key
+    const encryptionKey = Crypto.getRandomValues(new Uint8Array(16));
+
+    // convert the value to bytes (UTF-8 to Uint8Array.)
+    const valueBytes = aesjs.utils.utf8.toBytes(value);
+    // counter CTR
+    const aesCtr = new aesjs.ModeOfOperation.ctr(encryptionKey);
+
+    // converting encryption key to hex string and storing in secure store
+    await SecureStore.setItemAsync(
+      key,
+      aesjs.utils.hex.fromBytes(encryptionKey)
+    );
+
+    // encrypt the value bytes
+    const encryptedBytes = aesCtr.encrypt(valueBytes);
+    // convert encrypted bytes to hex string
+    const encryptedValue = aesjs.utils.hex.fromBytes(encryptedBytes);
+    return encryptedValue;
+  }
+
+  private async _decrypt(key: string, value: string) {
+    // retrive hex key from secure store
+    const encryptionKey = await SecureStore.getItemAsync(key);
+    if (!encryptionKey) {
+      return null;
+    }
+    const encryptedKeyInBytes = aesjs.utils.hex.toBytes(encryptionKey);
+    // counter CTR
+    const aesCtr = new aesjs.ModeOfOperation.ctr(encryptedKeyInBytes);
+    const decryptedBytes = aesCtr.decrypt(aesjs.utils.hex.toBytes(value));
+
+    // Convert our bytes back into text
+    var decryptedValue = aesjs.utils.utf8.fromBytes(decryptedBytes);
+    return decryptedValue;
   }
 }
 
